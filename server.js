87c6e5f823cdba25b51dabf0df24cde75868c4f0
janggeun.lee <longroot.lee@gmail.com>
2021-07-06 19:12:48 +0900

    채팅

--- server.js
+++ server.js
@@ -57,23 +57,49 @@ app.post('/',(req, res)=>{
 
 
 io.on('connection', socket => {
-
+    //음성인식
     socket.on("VoiceRe", (text, roomid)=>{
         socket.broadcast.to(roomid).emit("VoiceRe", text)
     } )
-    
+    //파파고 번역
     socket.on("papago", (lang, target)=>{
         socket.emit("papago", lang,instance.papagolangSync(target,lang))
     })
+
+    // 새로운 유저가 접속했을 경우 다른 소켓에게도 알려줌 
+    socket.on('newUser', function(name, roomid) {
+        // 소켓에 이름 저장해두기 
+        socket.name = name
+
+        // 모든 소켓에게 전송(brodcast는 나 빼고 다 전송)
+        socket.broadcast.to(roomid).emit('update', {type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
+        socket.emit('update', {type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
+
+    })
+    
+    // 전송한 메시지 받기 
+    socket.on('message', function(data, roomid) {
+        // 받은 데이터에 누가 보냈는지 이름을 추가 
+        data.name = socket.name
+        
+        console.log(data)
+
+        //보낸 사람을 제외한 나머지 유저에게 메시지 전송 
+        socket.broadcast.to(roomid).emit('update', data);
+    })
+
 
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId)
         socket.broadcast.to(roomId).emit('user-connected', userId)
 
         socket.on('disconnect', () => {
+            socket.broadcast.emit('update', {type: 'disconnect', name: 'SERVER', message: socket.name + '님이 나가셨습니다.'});
             socket.broadcast.to(roomId).emit('user-disconnected', userId)
         })
     })
+
+    
 })
 
 server.listen(3000)
