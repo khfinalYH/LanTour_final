5e893600460104cc255e6dd6bec85027e0d01763
Dressro <chlxowns55@gmail.com>
2021-07-12 19:01:41 +0900

    수정

--- src/main/java/com/lan/tour/model/biz/MemberBiz.java
+++ src/main/java/com/lan/tour/model/biz/MemberBiz.java
@@ -9,23 +9,17 @@ public interface MemberBiz {
 	public List<MemberDto> selectList();
 
 	public MemberDto selectOne(int member_no);
-<<<<<<< HEAD
-=======
 
 	public MemberDto idCheck(MemberDto dto);
 
->>>>>>> e857c90a92ec46e50b0379898bd407c322de1b6c
 	public int update(MemberDto dto);
 
 	public int delete(int member_no);
 
 	public int insert(MemberDto dto);
-<<<<<<< HEAD
-	public MemberDto idCheck(MemberDto dto);
-=======
 
 	public List<MemberDto> memberList();
 
 	public int memberupdate(MemberDto dto);
->>>>>>> e857c90a92ec46e50b0379898bd407c322de1b6c
+
 }
